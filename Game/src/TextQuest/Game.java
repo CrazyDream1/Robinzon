66fc098017bef2eedd7d912e23a42d0b1e72adbd
MMGGAASS <82602042+MMGGAASS@users.noreply.github.com>
2021-04-20 16:31:02 +0300

    Я написал слова

--- Game/src/TextQuest/Game.java
+++ Game/src/TextQuest/Game.java
@@ -38,7 +38,7 @@ public class Game {
         }
     }
 
-    private void playForest(){
+    private void playForest(){System.out.println("Вы пришли в локацию лес");
         if (check() != 0) return;
     }
 
